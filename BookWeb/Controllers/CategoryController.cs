71d80744e87d60202d3aa2676b03751a9c8168ef
Sidney Shafer <sidney@mnshafers.com>
2024-05-12 07:28:20 -0500

    Add Functionality to Update a Category

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -57,13 +57,9 @@ namespace BookWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("Name", "The Display Order cannot match Category Name.");
-            }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }

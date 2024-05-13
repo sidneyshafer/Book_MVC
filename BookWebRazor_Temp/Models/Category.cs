c47010266da5474359c26f479b5e21ad9a450c80
Sidney Shafer <sidney@mnshafers.com>
2024-05-13 05:36:29 -0500

    Create Category Class in Razor Project

---
+++ BookWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BookWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "The Category Name field must be less than 30 characters.")]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "The Display Order field is required.")]
+        [Range(1, 100, ErrorMessage = "The Display Order field must be between 1-100.")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}

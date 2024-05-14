7d3ad94e1f47493b44584156671241e5d1a41ed2
Sidney Shafer <sidney@mnshafers.com>
2024-05-14 06:23:12 -0500

    Add a Create Category Post Handler

--- BookWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BookWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -5,17 +5,28 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookWebRazor_Temp.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+
+        //[BindProperty]
         public Category Category { get; set; }
 
         public CreateModel(ApplicationDbContext db)
         {
             _db = db;  
         }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }

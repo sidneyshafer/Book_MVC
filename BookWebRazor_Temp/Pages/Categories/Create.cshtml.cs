855e4728523e757698aa367c9c5d1f101ba3a446
Sidney Shafer <sidney@mnshafers.com>
2024-05-14 06:13:57 -0500

    Add Page for Creating a New Category

---
+++ BookWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,21 @@
+using BookWebRazor_Temp.Data;
+using BookWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookWebRazor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;  
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
